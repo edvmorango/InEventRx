976294e0138410c5e4645c57e64389136e1d91d7
José Eduardo Vieira Morango <edvmorango@gmail.com>
2017-01-12 10:22:25 -0200

    Initial Commit

---
+++ InEventRx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InEventRx
+//
+//  Created by José Eduardo Vieira Morango on 12/01/17.
+//  Copyright © 2017 José Eduardo Vieira Morango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
